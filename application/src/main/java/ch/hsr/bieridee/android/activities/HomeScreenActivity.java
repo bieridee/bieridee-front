75a5d313f4f76644aa67a11d4c41f80f4743b276
Chrigi Faessler <cfaessle@hsr.ch>
2012-04-24 15:35:02 +0200

    Erstellung Dashboard Activity.

---
+++ application/src/main/java/ch/hsr/bieridee/android/activities/HomeScreenActivity.java
@@ -0,0 +1,63 @@
+package ch.hsr.bieridee.android.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import ch.hsr.bieridee.android.R;
+
+/**
+ * Activity that shows a list of all beers in our database.
+ */
+public class HomeScreenActivity extends Activity {
+
+	private static final String TAG = "bieridee-front-homescreen";
+
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being shut down then this Bundle contains the
+	 *            data it most recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.i(TAG, "onCreate");
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.homescreen);
+		this.addOnClickListener((Button) findViewById(R.id_dashboardscreen.buttonBeerlist), BeerListActivity.class);
+	}
+
+	private void addOnClickListener(Button button, final Class<? extends Activity> activityClass) {
+		button.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				final Intent intent = new Intent(v.getContext(), activityClass);
+				startActivityForResult(intent, 0);
+
+			}
+		});
+	}
+
+	@Override
+	public void onStart() {
+		Log.i(TAG, "onStart");
+		super.onStart();
+	}
+
+	@Override
+	public void onPause() {
+		Log.i(TAG, "onPause");
+		super.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		Log.i(TAG, "onResume");
+		super.onResume();
+	}
+
+}
