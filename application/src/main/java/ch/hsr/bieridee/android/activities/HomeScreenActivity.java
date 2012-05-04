c8cec3614c574a49d21b38ae5ecf4d935d1990dc
Danilo Bargen <gezuru@gmail.com>
2012-05-04 23:21:38 +0200

    Removed login activity launcher

--- application/src/main/java/ch/hsr/bieridee/android/activities/HomeScreenActivity.java
+++ application/src/main/java/ch/hsr/bieridee/android/activities/HomeScreenActivity.java
@@ -3,7 +3,6 @@ package ch.hsr.bieridee.android.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
