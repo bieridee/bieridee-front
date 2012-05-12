4726574cd542909fac3a7a6e73609f200a33d551
Danilo Bargen <gezuru@gmail.com>
2012-05-12 02:01:55 +0200

    Support for POST entity

--- application/src/main/java/ch/hsr/bieridee/android/http/HttpHelper.java
+++ application/src/main/java/ch/hsr/bieridee/android/http/HttpHelper.java
@@ -5,12 +5,19 @@ import ch.hsr.bieridee.android.exceptions.BierIdeeException;
 import ch.hsr.bieridee.android.http.requestprocessors.IRequestProcessor;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.*;
 import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.entity.AbstractHttpEntity;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.codehaus.jackson.JsonParser;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -53,12 +60,45 @@ public final class HttpHelper {
 	}
 
 	/**
-	 * Perform a POST request.
+	 * Perform a POST request without body data.
 	 * @param uri Full URI of the server resource
 	 * @return A HttpResponse instance
 	 */
 	public HttpResponse post(String uri) {
-		HttpRequestBase request = applyRequestProcessors(new HttpPost(uri));
+		return this.post(uri, null, null);
+	}
+
+	/**
+	 * Perform a POST request with attached JSONObject entity.
+	 * @param uri Full URI of the server resource
+	 * @param data A NameValuePair array containing
+	 * @return A HttpResponse instance
+	 */
+	public HttpResponse post(String uri, JSONObject data) {
+		try {
+			StringEntity stringEntity = new StringEntity(data.toString(2));
+			return this.post(uri, stringEntity, "application/json");
+		} catch (UnsupportedEncodingException e) {
+			throw new BierIdeeException("Unsupported encoding for POST body data.", e);
+		} catch(JSONException e){
+			throw new BierIdeeException("Could not properly decode JSON body data.", e);
+		}
+	}
+
+	/**
+	 * Perform a POST request with attached entity.
+	 * @param uri Full URI of the server resource
+	 * @param data A NameValuePair array containing
+	 * @return A HttpResponse instance
+	 */
+	public HttpResponse post(String uri, AbstractHttpEntity data, String contentType) {
+		HttpPost request = (HttpPost) applyRequestProcessors(new HttpPost(uri));
+		if (data != null) {
+			request.setEntity(data);
+		}
+		if (contentType != null) {
+			request.setHeader("Content-type", contentType);
+		}
 		return this.execute(request);
 	}
 
