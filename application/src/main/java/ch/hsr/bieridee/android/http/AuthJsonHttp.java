f58d079a4b4eb56fb500e421951b8e0d690e6365
Danilo Bargen <gezuru@gmail.com>
2012-05-14 13:15:26 +0200

    Added a new create method in AuthJsonHttp

--- application/src/main/java/ch/hsr/bieridee/android/http/AuthJsonHttp.java
+++ application/src/main/java/ch/hsr/bieridee/android/http/AuthJsonHttp.java
@@ -24,4 +24,17 @@ public final class AuthJsonHttp {
 		httpHelper.addRequestProcessor(new HMACAuthRequestProcessor());
 		return httpHelper;
 	}
+
+	/**
+	 * Create a new HttpHelper instance, add the AcceptRequestProcessor
+	 * and the HMACAuthRequestProcessor with cusotm login data to it
+	 * and return it.
+	 * @return HttpHelper instance
+	 */
+	public static HttpHelper create(String username, String password) {
+		final HttpHelper httpHelper = new HttpHelper();
+		httpHelper.addRequestProcessor(new AcceptRequestProcessor(AcceptRequestProcessor.ContentType.JSON));
+		httpHelper.addRequestProcessor(new HMACAuthRequestProcessor(username, password));
+		return httpHelper;
+	}
 }
