d2c7721fa50619b6d0f0c9a1cb253a5e0a7a04ef
Chrigi Faessler <cfaessle@hsr.ch>
2012-05-19 10:58:17 +0200

    Erstellung konktextmenu + delete action

--- application/src/main/java/ch/hsr/bieridee/android/activities/BeerListActivity.java
+++ application/src/main/java/ch/hsr/bieridee/android/activities/BeerListActivity.java
@@ -1,27 +1,33 @@
 package ch.hsr.bieridee.android.activities;
 
-import android.app.ListActivity;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import ch.hsr.bieridee.android.R;
-import ch.hsr.bieridee.android.adapters.BeerListAdapter;
-import ch.hsr.bieridee.android.config.Res;
-import ch.hsr.bieridee.android.http.AuthJsonHttp;
-import ch.hsr.bieridee.android.http.HttpHelper;
+import java.io.IOException;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.io.IOException;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import ch.hsr.bieridee.android.R;
+import ch.hsr.bieridee.android.adapters.BeerListAdapter;
+import ch.hsr.bieridee.android.config.Res;
+import ch.hsr.bieridee.android.http.AuthJsonHttp;
+import ch.hsr.bieridee.android.http.HttpHelper;
 
 /**
  * Activity that shows a list of all beers in our database.
@@ -43,12 +49,14 @@ public class BeerListActivity extends ListActivity {
 		this.adapter = new BeerListAdapter(this);
 		setListAdapter(this.adapter);
 		this.addOnClickListeners();
+		this.registerForContextMenu(this.getListView());
+		this.addOnLongClickListeners();
 	}
 
 	@Override
 	public void onStart() {
 		super.onStart();
-		new GetBeerData().execute();
+		new DeleteBeer().execute();
 	}
 
 	@Override
@@ -69,14 +77,58 @@ public class BeerListActivity extends ListActivity {
 	}
 
 	/**
+	 * Add onClick listeners to UI elements.
+	 */
+	private void addOnClickListeners() {
+		this.getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				final Intent intent = new Intent(view.getContext(), BeerDetailActivity.class);
+				intent.putExtra(BeerDetailActivity.EXTRA_BEER_ID, id);
+				startActivity(intent);
+			}
+		});
+	}
+
+	private void addOnLongClickListeners() {
+		this.getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				Log.d("long press", "long press was performed " + arg0 + " " + arg1 + " " + arg2 + " " + arg3);
+				return true;
+			}
+
+		});
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.setHeaderTitle(BeerListActivity.this.getString(R.string.beerlistContextTitle));
+		menu.add(0, v.getId(), 0, BeerListActivity.this.getString(R.string.beerlistContextDelete));
+		menu.add(0, v.getId(), 0, BeerListActivity.this.getString(R.string.beerlistContextEdit));
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		if (item.getTitle() == BeerListActivity.this.getString(R.string.beerlistContextDelete)) {
+			new DeleteBeer().execute(item.getItemId());
+		} else if (item.getTitle() == BeerListActivity.this.getString(R.string.beerlistContextEdit)) {
+			// function2(item.getItemId());
+		} else {
+			return false;
+		}
+		return true;
+	}
+
+	/**
 	 * Async task to get beers from server and update UI.
 	 */
 	private class GetBeerData extends AsyncTask<Void, Void, JSONArray> {
 		@Override
 		protected void onPreExecute() {
-			BeerListActivity.this.progressDialog = ProgressDialog.show(
-					BeerListActivity.this, getString(R.string.pleaseWait), getString(R.string.loadingData), true);
+			BeerListActivity.this.progressDialog = ProgressDialog.show(BeerListActivity.this, getString(R.string.pleaseWait), getString(R.string.loadingData), true);
 		}
+
 		@Override
 		protected JSONArray doInBackground(Void... voids) {
 			final HttpResponse response = BeerListActivity.this.httpHelper.get(Res.getURI(Res.BEER_COLLECTION));
@@ -96,6 +148,7 @@ public class BeerListActivity extends ListActivity {
 			}
 			return null;
 		}
+
 		@Override
 		protected void onPostExecute(JSONArray result) {
 			if (result != null) {
@@ -107,15 +160,35 @@ public class BeerListActivity extends ListActivity {
 	}
 
 	/**
-	 * Add onClick listeners to UI elements.
+	 * Async task to delete beer from server and update UI.
 	 */
-	private void addOnClickListeners() {
-		this.getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				final Intent intent = new Intent(view.getContext(), BeerDetailActivity.class);
-				intent.putExtra(BeerDetailActivity.EXTRA_BEER_ID, id);
-				startActivity(intent);
+	private class DeleteBeer extends AsyncTask<Integer, Void, Void> {
+		@Override
+		protected void onPreExecute() {
+			BeerListActivity.this.progressDialog = ProgressDialog.show(BeerListActivity.this, getString(R.string.pleaseWait), getString(R.string.loadingData), true);
+		}
+
+		@Override
+		protected Void doInBackground(Integer... ids) {
+			BeerListActivity.this.httpHelper.delete(Res.BEER_COLLECTION);
+			final HttpResponse response = BeerListActivity.this.httpHelper.get(Res.getURI(Res.BEER_COLLECTION + "?beerid=" + ids[0]));
+
+			if (response != null) {
+				final int statusCode = response.getStatusLine().getStatusCode();
+				if (statusCode == HttpStatus.SC_OK) {
+				}
 			}
-		});
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			if (result != null) {
+				new GetBeerData().execute();
+				// TODO maybe dont update the whole list on delete of one item...
+			} // TODO handle else
+			BeerListActivity.this.progressDialog.dismiss();
+		}
 	}
+
 }
