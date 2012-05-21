59d3c1bf9a2fd32bcd19c813c2527e98eee00cd7
Jonas Furrer <jfurrer@hsr.ch>
2012-05-21 16:18:20 +0200

    brauhaus

--- application/src/main/java/ch/hsr/bieridee/android/config/Res.java
+++ application/src/main/java/ch/hsr/bieridee/android/config/Res.java
@@ -9,8 +9,7 @@ public final class Res {
 		// do not instantiate.
 	}
 
-	//public static final String API_URL = "http://brauhaus.nusszipfel.com:8080";
-	public static final String API_URL = "http://152.96.236.93:8080";
+	public static final String API_URL = "http://brauhaus.nusszipfel.com:8080";
 
 	// beer
 	public static final String BEER_COLLECTION = "/beers";
